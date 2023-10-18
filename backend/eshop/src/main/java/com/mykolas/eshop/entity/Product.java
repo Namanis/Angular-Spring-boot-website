ccc84ee2b40afa9e6ff47ff49259de15142581ae
Namanis <mykolas.skrodenis@gmail.com>
2023-10-18 20:21:22 +0300

    Innit

---
+++ backend/eshop/src/main/java/com/mykolas/eshop/entity/Product.java
@@ -0,0 +1,47 @@
+package com.mykolas.eshop.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unit_price;
+
+    @Column(name = "image_url")
+    private String image_url;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    private Date dateCreated;
+
+    @Column(name = "date_updated")
+    private Date dateUpdated;
+
+}
---
+++ backend/eshop/src/test/java/com/mykolas/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykolas.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
