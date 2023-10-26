df5df09cddc3edde8e2be1d1c48a8d3f504bb206
Namanis <55447388+Namanis@users.noreply.github.com>
2023-10-26 21:49:04 +0000

    commit

--- frontend/angular-eshop/src/app/common/product.ts
+++ frontend/angular-eshop/src/app/common/product.ts
@@ -1,2 +1,11 @@
 export class Product {
+    constructor (public sku: string,
+                public name: string,
+                public description: string,
+                public unitPrice: number,
+                public imageUrl: string,
+                public active: boolean,
+                public unitInStock: number,
+                public dateCreated: Date,
+                public lastUpdated: Date){}
 }
