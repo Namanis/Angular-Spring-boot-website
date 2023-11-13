add62a953c7f410a723689f575ae576b06bce3fc
Namanis <55447388+Namanis@users.noreply.github.com>
2023-11-13 05:44:57 +0000

    commit

--- frontend/angular-eshop/src/app/app.module.ts
+++ frontend/angular-eshop/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { ProductListComponent } from './component/product-list/product-list.comp
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductCategoryMenuComponent } from './component/product-category-menu/product-category-menu.component';
 
 
 const routes: Routes = [
-  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category/:id/:name', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
   {path: '', redirectTo: '/products', pathMatch: 'full'},
@@ -20,7 +21,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
--- frontend/angular-eshop/src/app/component/product-list/product-list.component.ts
+++ frontend/angular-eshop/src/app/component/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number =1;
+  currentCategoryName: string = "";
   
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -26,11 +27,17 @@ export class ProductListComponent implements OnInit {
     //check if 'id' parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
-    if(hasCategoryId){
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+ 
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
     }
-    else{
+    else {
+      // not category id available ... default to category id 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
 
